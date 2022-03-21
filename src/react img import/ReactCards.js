3731409b5ab77afb30b78841477f440c723dc80b
manojkumar8498072171 <gollapallimanojkumar.gm@gmail.com>
2022-03-21 09:27:30 +0530

    React cards

---
+++ src/react img import/ReactCards.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import taj from './taj.jpg'
+import gol from './gol.jpg'
+import char from './char.jpg'
+
+const ReactCards = () => {
+  return (
+    <>
+      <div className='container mt-3'>
+        <h2 className='text-danger'>React cards</h2>
+          <div className='row'>
+              <div className='col-md-3'>
+                  <div className='card shadow-lg'>
+                         <img src={taj} alt='taj'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>taj</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={gol} alt='gol'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>gol</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={char} alt='char'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>char</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={taj} alt='taj'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>taj</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div> 
+
+ <div className='col-md-3'>
+                  <div className='card shadow-lg'>
+                         <img src={taj} alt='taj'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>taj</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={gol} alt='gol'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>gol</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. iyujhyjh illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={char} alt='char'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>char</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>
+              <div className='col-sm-3'>
+                  <div className='card shadow-lg'>
+                         <img src={taj} alt='taj'/>
+                      <div className='card-body'>
+                         <h1 className='text-danger'>taj</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. praesentium commodi beatae delectus illum rerum doloremque dese, aliquid ducimus id quidem impedit!</p>
+                      <button className='btn btn-dark'>Read more</button>
+                      </div>
+                  </div>
+              </div>                                                            
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default ReactCards
